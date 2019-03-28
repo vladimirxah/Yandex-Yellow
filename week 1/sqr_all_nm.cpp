912b3baf3b19fb946508d06f125f6b02f55f3026
vladimirxah <48546788+vladimirxah@users.noreply.github.com>
2019-03-28 11:32:45 +0300

    Add files via upload
    Принятое решение

--- week 1/sqr_all_nm.cpp
+++ week 1/sqr_all_nm.cpp
@@ -2,59 +2,8 @@
 #include <vector>
 #include <map>
 #include <utility>
-//#include <set>
-//#include <iterator>
-//#include <string>
 
 using namespace std;
-
-/*
- * Реализуйте шаблонную функцию Sqr, которая работает не только для чисел, но и для контейнеров. Функция должна возвращать копию исходного контейнера, модифицировав его следующим образом:
-
-для vector элементы нужно возвести в квадрат;
-для map в квадрат нужно возвести только значения, но не ключи;
-для pair в квадрат нужно возвести каждый элемент пары.
-// Пример вызова функции
-vector<int> v = {1, 2, 3};
-cout << "vector:";
-for (int x : Sqr(v)) {
-  cout << ' ' << x;
-}
-cout << endl;
-map<int, pair<int, int>> map_of_pairs = {
-  {4, {2, 2}},
-  {7, {4, 3}}
-};
-cout << "map of pairs:" << endl;
-for (const auto& x : Sqr(map_of_pairs)) {
-  cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
-}
-//Код выше должен вывести
-vector: 1 4 9
-map of pairs:
-4 4 4
-7 16 9
-
-Функция должна корректно работать не только для контейнеров, состоящих из чисел, но и для составных объектов, например, векторов словарей пар чисел.
-
-Для успешной сдачи решения необходимо сделать предварительное объявление шаблонных функций перед всеми шаблонными функциями.
-
-Пример предварительного объявления шаблонной функции
-// Предварительное объявление шаблонных функций
-template<typename T> T FuncA(T x);
-template<typename T> void FuncB(T x);
-
-// Объявляем шаблонные функции
-template <typename T>
-T FuncA(T x) { /.../ }
-
-template <typename T>
-void FuncB(T x) { /.../ }
- *
- *
- *
- *
- */
 
 template <typename T> T Sqr(const T& x);
 template <typename T> vector<T> Sqr(const vector<T>& x);
@@ -95,3 +44,57 @@ First f = Sqr(p1.first);
 Second s = Sqr(p1.second);
 return {f, s};
 }
+
+
+//Образец
+
+/*// Объявим функции без тела,
+// чтобы их можно было использовать в произвольном порядке
+
+// Для простых типов
+template<typename T>
+T Sqr(T value);
+
+// Для вектора
+template<typename T>
+vector<T> Sqr(const vector<T>& value);
+
+// Для словаря
+template<typename K, typename V>
+map<K, V> Sqr(const map<K, V>& value);
+
+// Для пары
+template<typename K, typename V>
+pair<K, V> Sqr(const pair<K, V>& value);
+
+// Теперь реализуем их в соответствии с заданием
+template<typename T>
+T Sqr(T value) {
+  return value * value;
+}
+
+template<typename T>
+vector<T> Sqr(const vector<T>& value) {
+  vector<T> result;
+  for (const T& x : value) {
+    // Здесь важно, что вместо x * x вызывается функция Sqr:
+    // это позволяет возводить в квадрат составные объекты
+    result.push_back(Sqr(x));
+  }
+
+  return result;
+}
+
+template<typename K, typename V>
+map<K, V> Sqr(const map<K, V>& value) {
+  map<K, V> result;
+  for (const auto& element : value) {
+    result[element.first] = Sqr(element.second);
+  }
+  return result;
+}
+
+template<typename K, typename V>
+pair<K, V> Sqr(const pair<K, V>& value) {
+  return {Sqr(value.first), Sqr(value.second)};
+}*/
