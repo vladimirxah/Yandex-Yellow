ec6e02120868e91a75fe7a9dd32c765c89bd72f9
vladimirxah <48546788+vladimirxah@users.noreply.github.com>
2019-04-12 11:59:37 +0300

    uploaded header file function realising

---
+++ week 3/sum_reverse_sort.cpp
@@ -0,0 +1,54 @@
+/*
+ * sum_reverse_sort.cpp
+ *
+ *  Created on: 12 апр. 2019 г.
+ *      Author: goncharov
+ */
+
+/*
+Вам дан заголовочный файл sum_reverse_sort.h, содержащий объявления трёх функций
+Вам надо прислать cpp-файл, содержащий определения этих функций.
+*/
+
+
+
+#include <algorithm>
+#include "sum_reverse_sort.h"
+
+int Sum(int x, int y) {
+	return x + y;
+}
+
+string Reverse(string s) {
+	string rev;
+	for (size_t i = s.size(); i > 0; --i) {
+		rev.push_back(s[i - 1]);
+	}
+	return rev;
+}
+
+void Sort(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
+}
+
+
+//EXAMPLE
+
+/*
+#include "sum_reverse_sort.h"
+
+#include <algorithm>
+using namespace std;
+
+int Sum(int x, int y) {
+  return x + y;
+}
+
+string Reverse(string s) {
+  reverse(s.begin(), s.end());
+  return s;
+}
+
+void Sort(vector<int>& nums) {
+  sort(nums.begin(), nums.end());
+}*/
