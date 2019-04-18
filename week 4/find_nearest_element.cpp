a665be25bfc22de38398ccac60955124afd28c72
vladimirxah <48546788+vladimirxah@users.noreply.github.com>
2019-04-18 10:36:43 +0300

    решение необязательной задачи «Поиск ближайшего элемента»

---
+++ week 4/find_nearest_element.cpp
@@ -0,0 +1,145 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 12 апр. 2019 г.
+ *      Author: goncharov
+ *
+Напишите функцию FindNearestElement, для множества целых чисел numbers и данного числа border возвращающую итератор на элемент множества, ближайший к border. Если ближайших элементов несколько, верните итератор на наименьший из них.
+
+set<int>::const_iterator FindNearestElement(
+    const set<int>& numbers,
+    int border);
+// set<int>::const_iterator —
+// тип итераторов для константного множества целых чисел
+ */
+
+#include <iostream>
+//#include <vector>
+#include <set>
+#include <algorithm>
+//#include <iterator>
+//#include <string>
+
+#include "test_runner.h"
+
+using namespace std;
+
+set<int>::const_iterator FindNearestElement(const set<int>& numbers, int border);
+// set<int>::const_iterator —
+// тип итераторов для константного множества целых чисел
+
+set<int>::const_iterator FindNearestElement(const set<int>& numbers, int border) {
+  // Запрашиваем итератор на первый элемент, не меньший border;
+  // если такого элемента нет, то мы получим numbers.end()
+  // set::lower_bound ->
+  // http://ru.cppreference.com/w/cpp/container/set/lower_bound
+  const auto first_not_less = numbers.lower_bound(border);
+
+  // Если множество пусто или до первого элемента не меньше border
+  // нет элементов, то мы уже получили ответ
+  if (first_not_less == numbers.begin()) {
+    return first_not_less;
+  }
+
+  // Если элементов, не меньших border, нет и set не пуст, то достаточно взять
+  // итератор на последний элемент, меньший border
+  // prev -> http://ru.cppreference.com/w/cpp/iterator/prev
+  const auto last_less = prev(first_not_less);
+  if (first_not_less == numbers.end()) {
+    return last_less;
+  }
+
+  // Разыменуем оба итератора-кандидата и выберем тот,
+  // чей элемент ближе к искомому
+  const bool is_left = (border - *last_less <= *first_not_less - border);
+  return is_left ? last_less : first_not_less;
+}
+
+void TestWorks() {
+//	AssertEqual(PrintVectorPart({6, 1, 8, -5, 4}), "8 1 6", "Just to check that Assert works");
+//	AssertEqual(PrintVectorPart({-6, 1, 8, -5, 4}), "", "Just to check that Assert works");
+//	AssertEqual(PrintVectorPart({6, 1, 8, 5, 4}), "4 5 8 1 6", "Just to check that Assert works");
+}
+
+
+int main() {
+
+  set<int> numbers = {1, 4, 6};
+  cout <<
+      *FindNearestElement(numbers, 0) << " " <<
+      *FindNearestElement(numbers, 3) << " " <<
+      *FindNearestElement(numbers, 5) << " " <<
+      *FindNearestElement(numbers, 6) << " " <<
+      *FindNearestElement(numbers, 100) << endl;
+
+  set<int> empty_set;
+
+  cout << (FindNearestElement(empty_set, 8) == end(empty_set)) << endl;
+
+//  cout << "Correct:\n1 4 4 6 6\n1";
+return 0;
+}
+
+
+//example
+/*
+В этой задаче достаточно аккуратно реализовать предложенный алгоритм, не ошибившись при использовании итераторов.
+*/
+/*Часть 1. Разбиение на 2 части
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end) {
+  // 1. Если диапазон содержит меньше 2 элементов, выходим из функции
+  int range_length = range_end - range_begin;
+  if (range_length < 2) {
+    return;
+  }
+
+  // 2. Создаем вектор, содержащий все элементы текущего диапазона
+  vector<typename RandomIt::value_type> elements(range_begin, range_end);
+
+  // 3. Разбиваем вектор на две равные части
+  auto mid = elements.begin() + range_length / 2;
+
+  // 4. Вызываем функцию MergeSort от каждой половины вектора
+  MergeSort(elements.begin(), mid);
+  MergeSort(mid, elements.end());
+
+  // 5. С помощью алгоритма merge сливаем отсортированные половины
+  // в исходный диапазон
+  // merge -> http://ru.cppreference.com/w/cpp/algorithm/merge
+  merge(elements.begin(), mid, mid, elements.end(), range_begin);
+}
+
+Часть 2. Разбиение на 3 части
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end) {
+  // 1. Если диапазон содержит меньше 2 элементов, выходим из функции
+  int range_length = range_end - range_begin;
+  if (range_length < 2) {
+    return;
+  }
+
+  // 2. Создаем вектор, содержащий все элементы текущего диапазона
+  vector<typename RandomIt::value_type> elements(range_begin, range_end);
+
+  // 3. Разбиваем вектор на три равные части
+  auto one_third = elements.begin() + range_length / 3;
+  auto two_third = elements.begin() + range_length * 2 / 3;
+
+  // 4. Вызываем функцию MergeSort от каждой трети вектора
+  MergeSort(elements.begin(), one_third);
+  MergeSort(one_third, two_third);
+  MergeSort(two_third, elements.end());
+
+  // 5. С помощью алгоритма merge cливаем первые две трети во временный вектор
+  // back_inserter -> http://ru.cppreference.com/w/cpp/iterator/back_inserter
+  vector<typename RandomIt::value_type> interim_result;
+  merge(elements.begin(), one_third, one_third, two_third,
+        back_inserter(interim_result));
+
+  // 6. С помощью алгоритма merge сливаем отсортированные части
+  // в исходный диапазон
+  // merge -> http://ru.cppreference.com/w/cpp/algorithm/merge
+  merge(interim_result.begin(), interim_result.end(), two_third, elements.end(),
+        range_begin);
+}*/
