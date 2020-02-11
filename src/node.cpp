4bc560d75f1b1525dcdfd93e5ac077572c1b06f4
vladimirxah <48546788+vladimirxah@users.noreply.github.com>
2020-02-11 21:09:53 +0300

    Added files for class Node declaration

---
+++ src/node.cpp
@@ -0,0 +1,10 @@
+/*
+ * node.cpp
+ *
+ *  Created on: 11 февр. 2020 г.
+ *      Author: gonchrov
+ */
+
+#include "node.h"
+
+
---
+++ src/node.h
@@ -0,0 +1,5 @@
+/*классы Node, EmptyNode, DateComparisonNode, EventComparisonNode и LogicalOperationNode — сформировать их иерархию и публичный интерфейс вам поможет анализ функций main и ParseCondition;*/
+/*node.h/cpp — эти файлы должны содержать объявления и определения класса Node, а также всех его потомков (см. выше), которые представляют собой узлы абстрактного синтаксического дерева, формируемого функцией ParseCondition;*/
+
+#pragma once
+
