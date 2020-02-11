9936a0a787c56100faeecaf0ebfde2956cf1e4c9
vladimirxah <48546788+vladimirxah@users.noreply.github.com>
2020-02-11 21:31:45 +0300

    Added methods for database.h

--- src/database.h
+++ src/database.h
@@ -1,6 +1,8 @@
 /*класс Database, который представляет собой базу данных, — вы должны сами создать его публичный интерфейс, основываясь на том, как он используется в функции main*/
 /*database.h/cpp — эти файлы должны содержать объявление и определение класса Database;*/
 
+
+
 #pragma once
 
 #include "date.h"
@@ -14,9 +16,12 @@ using namespace std;
 
 class Database {
 public:
+	void Add (const Date& date, const string& event);
+	ostream Print (ostream& os);
+
 
 private:
 	map<Date, set<string>> db_set_;
 	map<Date, vector<string>> db_vec_;
 
-}
+};
