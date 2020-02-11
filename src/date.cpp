eaaeed54cd012d4bdc162faef655a69e993d0fee
vladimirxah <48546788+vladimirxah@users.noreply.github.com>
2020-02-11 21:17:20 +0300

    Copied realisation from White-Branch course project for class Date

--- src/date.cpp
+++ src/date.cpp
@@ -7,4 +7,25 @@
 
 #include "date.h"
 
+// конструктор выбрасывает исключение, если его аргументы некорректны
+Date::Date(int new_year, int new_month, int new_day) {
+	year = new_year;
+	if (new_month > 12 || new_month < 1) {
+		throw logic_error("Month value is invalid: " + to_string(new_month));
+	}
+	month = new_month;
+	if (new_day > 31 || new_day < 1) {
+		throw logic_error("Day value is invalid: " + to_string(new_day));
+	}
+	day = new_day;
+}
 
+int Date::GetYear() const {
+	return year;
+}
+int Date::GetMonth() const {
+	return month;
+}
+int Date::GetDay() const {
+	return day;
+}
--- src/date.h
+++ src/date.h
@@ -2,3 +2,37 @@
 /*date.h/cpp — эти файлы должны содержать объявления и определения класса Date, функции ParseDate и оператора вывода в поток для класса Date;*/
 
 #pragma once
+
+#include <stdexcept>
+#include <sstream>
+#include <iomanip>
+
+class Date {
+public:
+  Date(int new_year, int new_month, int new_day);
+
+  int GetYear() const;
+  int GetMonth() const;
+  int GetDay() const;
+
+private:
+  int year;
+  int month;
+  int day;
+};
+
+// определить сравнение для дат необходимо для использования их в качестве ключей словаря
+bool operator<(const Date& lhs, const Date& rhs) {
+  // воспользуемся тем фактом, что векторы уже можно сравнивать на <:
+  // создадим вектор из года, месяца и дня для каждой даты и сравним их
+	return make_tuple(lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()) <
+				 make_tuple(rhs.GetYear(), rhs.GetMonth(), rhs.GetDay());
+}
+
+// даты будут по умолчанию выводиться в нужном формате
+ostream& operator<<(ostream& stream, const Date& date) {
+  stream << setw(4) << setfill('0') << date.GetYear() <<
+      "-" << setw(2) << setfill('0') << date.GetMonth() <<
+      "-" << setw(2) << setfill('0') << date.GetDay();
+  return stream;
+}
