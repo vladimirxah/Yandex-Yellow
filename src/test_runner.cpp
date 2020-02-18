caa715c2eddf8e89d909a0f35c14198c6ee4c1f6
vladimirxah <48546788+vladimirxah@users.noreply.github.com>
2020-02-18 19:14:20 +0300

    Moved template to header file

--- src/test_runner.cpp
+++ src/test_runner.cpp
@@ -15,17 +15,6 @@ vector<int> h;
   AssertEqual(b, true, hint);
 }*/
 
-template <class TestFunc>
-void TestRunner::RunTest(TestFunc func, const string& test_name) {
-	try {
-		func();
-		cerr << test_name << " OK" << endl;
-	} catch (runtime_error& e) {
-		++fail_count;
-		cerr << test_name << " fail: " << e.what() << endl;
-	}
-}
-
 TestRunner::~TestRunner() {
 	if (fail_count > 0) {
 		cerr << fail_count << " unit tests failed. Terminate" << endl;
--- src/test_runner.h
+++ src/test_runner.h
@@ -83,8 +83,16 @@ void AssertEqual(const T& t, const U& u,
 
 class TestRunner {
 public:
-  template <class TestFunc>
-  void RunTest(TestFunc func, const string& test_name);
+	template <class TestFunc>
+	void RunTest(TestFunc func, const string& test_name) {
+		try {
+			func();
+			cerr << test_name << " OK" << endl;
+		} catch (runtime_error& e) {
+			++fail_count;
+			cerr << test_name << " fail: " << e.what() << endl;
+		}
+	}
 
   ~TestRunner();
 
